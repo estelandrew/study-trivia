fbd209da2ccea0fb5a7cc864a94a05041bfb4f71
Andrew Sims <andrewsims@Andrews-MacBook-Pro.local>
2023-10-19 16:59:18 -0400

    updated page header

--- src/app/categories/[category]/[slug]/page.tsx
+++ src/app/categories/[category]/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { getDeckBySlug } from "@lib/supabase";
 import PageSection from "@components/PageSection/PageSection";
 import PageHeader from "@components/PageHeader/PageHeader";
 import { GridDisplay } from "@components/GridDisplay/GridDisplay";
+import { ButtonWithDropdown } from "@components/ButtonWithDropdown/ButtonWithDropdown";
+import styles from "./page.module.scss";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const deck = await getDeckBySlug(params.slug);
@@ -9,8 +11,18 @@ export default async function Page({ params }: { params: { slug: string } }) {
     <>
       {/* TODO: figure out why Typescript is seems to be interpreting the returned data incorrectly. Possibly override data type returned from fetch? */}
       {/*@ts-ignore*/}
-      <PageHeader text={deck?.name} categorySlug={deck?.categories.slug} />
+      <PageHeader
+        text={deck?.name}
+        description={deck?.description}
+        categorySlug={deck?.categories.slug}
+      />
+      <div className={styles.row}>{deck?.cards.length} Rows</div>
       {/*@ts-ignore*/}
+      <div className={styles.toolbar}>
+        <ButtonWithDropdown text="View">
+          <div>Child container</div>
+        </ButtonWithDropdown>
+      </div>
       <GridDisplay deckData={deck} />
     </>
   );
