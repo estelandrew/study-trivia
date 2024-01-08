5982990454327547aade3c3ec6fc336762088fbc
Andrew Sims <andrewsims@Andrews-MacBook-Pro.local>
2024-01-08 15:21:42 -0500

    added deck filter button ui

--- src/components/DeckViewToolbar/DeckViewToolbar.tsx
+++ src/components/DeckViewToolbar/DeckViewToolbar.tsx
@@ -1,55 +1,20 @@
+import { ReactNode } from "react";
 import styles from "./DeckViewToolbar.module.scss";
 import ButtonWithDropdown from "@components/ButtonWithDropdown/ButtonWithDropdown";
-import { FaCheck } from "react-icons/fa";
-import { PiCardsBold } from "react-icons/pi";
-import { BsTable } from "react-icons/bs";
-import { DeckViews } from "@root/types";
+import { FaFilter } from "react-icons/fa";
+import { FilterDeckButton } from "@components/FilterDeckButton/FilterDeckButton";
 
 type PropsType = {
-  toggleView: (type: DeckViews) => void;
-  currentView: {
-    type: DeckViews;
-  };
+  foo: string;
+  children: ReactNode;
 };
 
-const DeckViewToolbar = ({ toggleView, currentView }: PropsType) => {
-  const viewItems = [
-    { type: DeckViews.Cards, label: "Flash Cards View", isSelected: true },
-    { type: DeckViews.Table, label: "Table View", isSelected: true },
-  ];
-
-  const Icon = currentView.type === DeckViews.Cards ? PiCardsBold : BsTable;
-  const label =
-    currentView.type === DeckViews.Cards
-      ? viewItems[0].label
-      : viewItems[1].label;
+const DeckViewToolbar = ({ foo, children }: PropsType) => {
+  const Icon = FaFilter;
 
   return (
     <div className={styles.toolbar}>
-      <ButtonWithDropdown
-        text="View"
-        icon={Icon}
-        label={label}
-        currentView={currentView}
-      >
-        <div className={styles.viewControl}>
-          {viewItems.map((item, i) => {
-            const isCurrentView = currentView.type === item.type;
-            return (
-              <div
-                key={item.type + i}
-                className={styles.viewControlItem}
-                onClick={() => toggleView(item.type)}
-              >
-                <div className={styles.viewControlItemCheckPlaceholder}>
-                  {isCurrentView && <FaCheck />}
-                </div>
-                <div>{item.label}</div>
-              </div>
-            );
-          })}
-        </div>
-      </ButtonWithDropdown>
+      <FilterDeckButton />
     </div>
   );
 };
